d8490c5a767466ecf43562202e807b7f2b445266
Maykel Santos <eumaykelandre@gmail.com>
2023-01-05 16:00:43 -0300

    sobre dicionários

---
+++ python_fundamentos_analise_dados/02_variaveis_tipos_estruturas_de_dados/02_dicionarios.py
@@ -0,0 +1,67 @@
+# criando um dicionário
+dict_1 = {
+    'Mateus': 24,
+    'Fernanda': 22,
+    'Tamires': 26,
+    'Cristiano': 25
+}
+
+# criando uma chave com valor no dicionário
+dict_1['Pedro'] = 23
+
+# apagando um dicionário
+dict_1.clear() 
+
+# deletando um dicionário
+del dict_1
+
+# criando novo dicionário
+dict_2 = {
+    'Mateus': 24,
+    'Fernanda': 22,
+    'Tamires': 26,
+    'Cristiano': 25,
+    'Pedro': 23
+}
+
+# identificando o tamanho do dicionário
+len(dict_2)
+
+# extraindo somente as chaves do dicionário
+dict_2.keys()
+
+# extraindo somente os valores do dicionãrio
+dict_2.values()
+
+# extraindo todos os itens do dicionário
+dict_2.items()
+
+# criando novo dicionário
+dict_3 = {
+    'Erika': 28,
+    'Maria': 27,
+    'Milton': 26
+}
+
+# atualizando os dados de um dicionário com outro dicionário
+dict_2.update(dict_3)
+
+# criando dicionário vazio
+dict_4 ={
+    
+}
+
+# adicionando itens ao dicionário
+dict_4['key_one'] = 2
+
+# adicionando novo item ao dicionário
+dict_4[10] = 5
+
+# adicionando novo item ao dicionário
+dict_4[7] = 'Jesus'
+
+# adicionando novo item ao dicionário
+dict_4['teste'] = 10
+
+# adicionando novo item ao dicionário
+dict_4[10] = 'teste'
